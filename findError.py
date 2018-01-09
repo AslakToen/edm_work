8de18058f5da664e61fc6fbf801b07e1cb12bc52
Aslak Tøn <ast@jotne.intra>
2018-01-09 14:26:28 +0100

    added output to own file

--- findError.py
+++ findError.py
@@ -1,8 +1,26 @@
-fh = open(("C:/Users/ast/work/edm1/edmbuild/edmcore_build_2017.all.out"), "r")
+storeFile = open(("C:/Users/ast/work/edm1/edmbuild/edmcore_build_2017.all.out"), "r")
 
-for line in fh:
+
+def num_in_line(line):
+    for i in range(0, 30):
+        if (str(i) + ">-") in line:
+            return True
+
+
+outputfile = ""
+
+for line in storeFile:
     if (" error ") in line:
-        #if not ("cs_errors" or "ei_xmlerror" or "") in line:
-        print line
+        outputfile += line
+    elif ("==========") in line:
+        outputfile += line
+    elif(num_in_line(line)):
+        outputfile += line
 
-fh.close();
+storeFile.close()
+
+output = open("output.txt", "a")
+
+output.write(outputfile)
+
+output.close()
