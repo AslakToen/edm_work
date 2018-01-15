e8d00e28de6d7962b8f6c9132b6f6b7c2fba6ed9
Aslak Tøn <ast@jotne.intra>
2018-01-15 13:03:04 +0100

    fixed bug where entries got coopied over

--- findError.py
+++ findError.py
@@ -3,27 +3,22 @@ storeFile = open(("C:/Users/ast/work/edm1/edmbuild/edmcore_build_2017.all.out"),
 outputfileDic = {}
 summaries = ""
 def num_in_line(line):
-    for i in range(0, 30):
-        temp = (str(i + 1) + ">-")
-        if (temp in line) and (temp[0] == line[0]) and (temp[1] == line[1]):
-            outputfileDic[i + 1] = [line]
+    for i in range(1, 31):
+        temp = (str(i) + ">-")
+        if (temp in line) and (temp[0] == line[0]) and (temp[1] == line[1]) and (not(i in outputfileDic.keys())):
+            outputfileDic[i] = [line]
 
 for line in storeFile:
     num_in_line(line)
 
     if (" error ") in line:
-        print(line)
         if(line[1] == ">"):
             outputfileDic[int(line[0:1])].append(line)
         else:
             outputfileDic[int(line[0:2])].append(line)
 
-            if(line[0:2] == str(28)):
-                print("printing 28")
-                print(outputfileDic[int(line[0:2])])
     elif ("==========") in line:
         summaries += line
-
 
 storeFile.close()
 
